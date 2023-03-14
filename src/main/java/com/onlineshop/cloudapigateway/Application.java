3dffe01fd386ca507ae12c6dc4293e17a0e807f1
Roman Behroz <romanbrkz@live.com>
2023-03-15 00:22:49 +0100

    added all the routes

--- src/main/java/com/onlineshop/cloudapigateway/Application.java
+++ src/main/java/com/onlineshop/cloudapigateway/Application.java
@@ -2,7 +2,9 @@ package com.onlineshop.cloudapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @SpringBootApplication()
 public class Application {
 
